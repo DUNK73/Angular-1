2a53fbe834503159f848c355fc0e551c0d129c35
DUNK <Z7dank3@yandex.ru>
2016-04-05 23:22:23 +0300

    Add project files.

---
+++ src/AngularTestApp/wwwroot/js/app.js
@@ -0,0 +1,34 @@
+﻿(function () {
+    'use strict';
+
+    var app = angular.module("AngularTest.App",
+    [
+        "AngularTest.Controllers",
+        "AngularTest.Services",
+        "AngularTest.Directives",
+
+        "ui.router",
+        "ngMaterial"
+    ]);
+
+    angular.module("AngularTest.Controllers", []);
+    angular.module("AngularTest.Services", []);
+    angular.module("AngularTest.Directives", []);
+
+    app.config(function ($stateProvider, $urlRouterProvider) {
+
+        $urlRouterProvider.otherwise("/screen1");
+
+        $stateProvider
+            .state('screen1', {
+                url: "/screen1",
+                templateUrl: "tpl-screen1"
+            });
+        $stateProvider
+            .state('screen2', {
+                url: "/screen2",
+                templateUrl: "tpl-screen2"
+            });
+    });
+
+})();
---
+++ src/AngularTestApp/wwwroot/js/controllers.js
@@ -0,0 +1,30 @@
+﻿(function() {
+    "use strict";
+
+    var app = angular.module("AngularTest.Controllers")
+        .controller("appController", function ($scope, $rootScope) {
+            $scope.model = { tabIndex: 0 }
+
+            $rootScope.$on("setTab", function(event,value) {
+                $scope.model.tabIndex = value;
+            });
+        })
+        .controller("screen1Controller", function ($scope, $rootScope, metadata) {
+
+            $scope.model = {
+                fields: metadata.fields
+            };
+
+            $rootScope.$broadcast("setTab", 0);
+
+        })
+        .controller("screen2Controller", function ($scope, $rootScope, metadata) {
+
+            $scope.model = {
+                fields: metadata.fields
+            };
+
+            $rootScope.$broadcast("setTab", 1);
+        });
+
+})();
